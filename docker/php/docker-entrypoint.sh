60b1d06814e96d05b73e549d743487d5ea38b802
Lorenzo <lorenzo.dessimoni@gmail.com>
2023-08-16 00:38:08 +0200

    Remove .gitignore and re add MERCURE environment variables.

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -10,19 +10,15 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'artisan' ]; then
 	# Install the project the first time PHP is started
 	# After the installation, the following block can be deleted
 	if [ ! -f composer.json ]; then
-	    echo "Download app at tmp/"
 		rm -Rf tmp/
 		composer create-project "laravel/laravel $LARAVEL_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install --no-scripts
 
-        echo "Copy app in root directory"
 		cd tmp
 		cp -Rp . ..
 		cd -
 
-        echo "Remove tmp/"
 		rm -Rf tmp/
 
-        echo "Run scripts"
         composer run-script post-root-package-install
 		composer install --prefer-dist --no-progress --no-interaction
 		composer run-script post-create-project-cmd
